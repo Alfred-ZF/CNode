3106934a59f59015d264c3b9a6140728385e1722
zf <413273029@qq.com>
2019-05-04 14:33:20 +0800

    添加 Header 和 PostList 模块

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <Header/> <br>
+    <div class="main">
+      <PostList/>
+    </div>
+    <PostList/>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header'
+  import PostList from './components/PostList'
+
+export default {
+  name: 'App',
+  components:{
+    Header,
+    PostList
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  
+}
+*::after{
+  box-sizing: border-box;
+}
+
+*::before{
+  box-sizing: border-box;
+}
+
+a{
+  text-decoration: none;
+}
+
+li{
+  list-style: none;
+}
+
+body{
+  background:#e1e1e1;
+}
+
+.main{
+  width: 80%;
+  margin:0 auto;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="header">
+    <div class="parent">
+      <div class="container clearfix">
+        <img src="../assets/cnodejs_light.svg" alt>
+        <ul>
+          <li>
+            <a href="#">首页</a>
+          </li>
+          <li>
+            <a href="#">新手入门</a>
+          </li>
+          <li>
+            <a href="#">API</a>
+          </li>
+          <li>
+            <a href="#">关于</a>
+          </li>
+          <li>
+            <a href="#">注册</a>
+          </li>
+          <li>
+            <a href="#">登录</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  height: 50px;
+  background-color: rgb(68, 68, 68);
+}
+.header .parent {
+  margin: 0 52px;
+}
+.header .parent .container {
+  height: 50px;
+  padding: 5px;
+}
+
+.header .parent .container img {
+  max-width: 120px;
+  /* margin-left: 50px; */
+  margin-top: 3px;
+}
+.clearfix::after {
+  content: "";
+  display: block;
+  clear: both;
+}
+
+.header .parent .container ul {
+  float: right;
+  height: 50px;
+  
+  margin-top: -5px;
+  margin-right: -35px;
+}
+
+.header .parent .container ul li {
+  display: inline-block;
+  height: 50px;
+  padding: 0px 15px;
+  /* line-height:50px; */
+  margin:0 -5px;
+}
+
+.header .parent .container ul li a {
+  display: inline-block;
+  font-size: 13px;
+  color: #ccc;
+  height: 50px;
+  line-height: 50px;
+  /* margin: -2px 0 0 0; */
+}
+</style>
